bf4664184e60add4f1da3e55a91e975ffcb53663
Justin Brunner <brunner.justin.jakob@gmail.com>
2017-03-29 21:37:23 -0600

    Got rid of username

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     var authToken;
-    var username;
 
     $("#postComment").click(function(){
         var myobj = { comment:$("#comment").val(), likes: 0, created_by:
